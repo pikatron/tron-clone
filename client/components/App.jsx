204a76f0ad63bbae3e67350868864f7dbaed9f9c
Dmitriyx <dmitriylevy01@gmail.com>
2019-08-19 16:08:15 -0400

    added basics of Google OAuth , there is a small bug to fix

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+
+      <div className="g-signin2" data-onsuccess="onSignIn"></div>
+    </div>
+  )
 }
 
 export default App;
